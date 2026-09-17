5dc5c4880b3830bc89550df31761559543ee9337
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 15:30:27 +0300

    fix(ui): inject missing R import in AddonManagerScreen.kt

--- CelestiaUI/src/main/java/space/celestia/celestiaui/resource/AddonManagerScreen.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/resource/AddonManagerScreen.kt
@@ -9,6 +9,8 @@
 
 package space.celestia.celestiaui.resource
 
+import space.celestia.mobilecelestia.R
+
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.togetherWith

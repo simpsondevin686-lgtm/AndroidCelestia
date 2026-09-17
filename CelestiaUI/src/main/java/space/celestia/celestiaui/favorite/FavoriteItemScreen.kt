ddabba63434d1bf99946842bb0f70e08dd5a0743
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 15:30:15 +0300

    fix(ui): inject missing R import in FavoriteItemScreen.kt

--- CelestiaUI/src/main/java/space/celestia/celestiaui/favorite/FavoriteItemScreen.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/favorite/FavoriteItemScreen.kt
@@ -9,6 +9,8 @@
 
 package space.celestia.celestiaui.favorite
 
+import space.celestia.mobilecelestia.R
+
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background

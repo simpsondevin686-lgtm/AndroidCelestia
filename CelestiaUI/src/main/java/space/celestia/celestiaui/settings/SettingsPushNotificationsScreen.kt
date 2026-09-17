d67c0cd64e18e2e51d48cbf872fe9b4ba69dce04
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 15:30:57 +0300

    fix(ui): inject missing R import in SettingsPushNotificationsScreen.kt

--- CelestiaUI/src/main/java/space/celestia/celestiaui/settings/SettingsPushNotificationsScreen.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/settings/SettingsPushNotificationsScreen.kt
@@ -9,6 +9,8 @@
 
 package space.celestia.celestiaui.settings
 
+import space.celestia.mobilecelestia.R
+
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager

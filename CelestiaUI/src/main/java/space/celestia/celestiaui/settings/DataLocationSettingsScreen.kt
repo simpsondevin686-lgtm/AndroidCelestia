2c6929e7c6aea44a29d39fc15ad9e2b8eb67a5d3
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 16:50:59 +0300

    revert: restore CelestiaUI/src/main/java/space/celestia/celestiaui/settings/DataLocationSettingsScreen.kt to initial fork state

--- CelestiaUI/src/main/java/space/celestia/celestiaui/settings/DataLocationSettingsScreen.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/settings/DataLocationSettingsScreen.kt
@@ -9,8 +9,6 @@
 
 package space.celestia.celestiaui.settings
 
-import space.celestia.mobilecelestia.R
-
 import android.content.Intent
 import android.provider.DocumentsContract
 import androidx.activity.compose.LocalActivity

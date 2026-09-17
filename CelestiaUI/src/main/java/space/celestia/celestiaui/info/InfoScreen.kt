a239bb6a27dbb5e3624b5f3fc4918e3751e9a2d7
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 15:30:21 +0300

    fix(ui): inject missing R import in InfoScreen.kt

--- CelestiaUI/src/main/java/space/celestia/celestiaui/info/InfoScreen.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/info/InfoScreen.kt
@@ -9,6 +9,8 @@
 
 package space.celestia.celestiaui.info
 
+import space.celestia.mobilecelestia.R
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues

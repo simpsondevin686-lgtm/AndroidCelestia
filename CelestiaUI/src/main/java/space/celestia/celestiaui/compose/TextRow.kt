cc0279983ba37461b34b3f96a1440628573b1394
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 16:50:08 +0300

    revert: restore CelestiaUI/src/main/java/space/celestia/celestiaui/compose/TextRow.kt to initial fork state

--- CelestiaUI/src/main/java/space/celestia/celestiaui/compose/TextRow.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/compose/TextRow.kt
@@ -1,7 +1,5 @@
 package space.celestia.celestiaui.compose
 
-import space.celestia.mobilecelestia.R
-
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*

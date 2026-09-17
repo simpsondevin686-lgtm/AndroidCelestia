85eafda57b0a05e5bace06bddbd82a4eecdedc09
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 15:29:55 +0300

    fix(ui): inject missing R import in CameraControl.kt

--- CelestiaUI/src/main/java/space/celestia/celestiaui/control/CameraControl.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/control/CameraControl.kt
@@ -9,6 +9,8 @@
 
 package space.celestia.celestiaui.control
 
+import space.celestia.mobilecelestia.R
+
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column

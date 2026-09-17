ea8eb25499adef3760d954d0fe3c3e1f84ad5f2c
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 15:30:42 +0300

    fix(ui): inject missing R import in SearchScreen.kt

--- CelestiaUI/src/main/java/space/celestia/celestiaui/search/SearchScreen.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/search/SearchScreen.kt
@@ -9,6 +9,8 @@
 
 package space.celestia.celestiaui.search
 
+import space.celestia.mobilecelestia.R
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box

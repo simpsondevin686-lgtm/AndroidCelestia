6f1e897e8df7e4ea33132815c248cefca8bfba0d
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 15:29:47 +0300

    fix(ui): inject missing R import in SliderRow.kt

--- CelestiaUI/src/main/java/space/celestia/celestiaui/compose/SliderRow.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/compose/SliderRow.kt
@@ -1,5 +1,7 @@
 package space.celestia.celestiaui.compose
 
+import space.celestia.mobilecelestia.R
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer

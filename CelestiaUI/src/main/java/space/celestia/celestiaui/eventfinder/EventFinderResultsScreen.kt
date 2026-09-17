e29b3f56201efbb693f30e61fbeeb8a79291d739
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 16:50:22 +0300

    revert: restore CelestiaUI/src/main/java/space/celestia/celestiaui/eventfinder/EventFinderResultsScreen.kt to initial fork state

--- CelestiaUI/src/main/java/space/celestia/celestiaui/eventfinder/EventFinderResultsScreen.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/eventfinder/EventFinderResultsScreen.kt
@@ -9,8 +9,6 @@
 
 package space.celestia.celestiaui.eventfinder
 
-import space.celestia.mobilecelestia.R
-
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues

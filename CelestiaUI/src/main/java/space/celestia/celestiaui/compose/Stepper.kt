56db1e67bf2c2062db9459c7a2a5030cf47fd865
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 16:50:01 +0300

    revert: restore CelestiaUI/src/main/java/space/celestia/celestiaui/compose/Stepper.kt to initial fork state

--- CelestiaUI/src/main/java/space/celestia/celestiaui/compose/Stepper.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/compose/Stepper.kt
@@ -1,7 +1,5 @@
 package space.celestia.celestiaui.compose
 
-import space.celestia.mobilecelestia.R
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource

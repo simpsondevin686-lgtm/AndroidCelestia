00dd8fb5ae640e43189cc58bbad376036b979206
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 16:49:55 +0300

    revert: restore CelestiaUI/src/main/java/space/celestia/celestiaui/compose/ResourceUtils.kt to initial fork state

--- CelestiaUI/src/main/java/space/celestia/celestiaui/compose/ResourceUtils.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/compose/ResourceUtils.kt
@@ -16,8 +16,6 @@
 
 package space.celestia.celestiaui.compose
 
-import space.celestia.mobilecelestia.R
-
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Resources

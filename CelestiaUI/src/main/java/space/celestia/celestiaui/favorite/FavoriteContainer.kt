75414ef423a171368297813ed90d713b09df5909
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 16:50:28 +0300

    revert: restore CelestiaUI/src/main/java/space/celestia/celestiaui/favorite/FavoriteContainer.kt to initial fork state

--- CelestiaUI/src/main/java/space/celestia/celestiaui/favorite/FavoriteContainer.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/favorite/FavoriteContainer.kt
@@ -9,8 +9,6 @@
 
 package space.celestia.celestiaui.favorite
 
-import space.celestia.mobilecelestia.R
-
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.togetherWith

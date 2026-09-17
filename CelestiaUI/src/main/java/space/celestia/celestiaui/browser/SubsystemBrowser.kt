ee19e5008b24e868c4b7a3c72e2b83649182600b
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 15:29:20 +0300

    fix(ui): inject missing R import in SubsystemBrowser.kt

--- CelestiaUI/src/main/java/space/celestia/celestiaui/browser/SubsystemBrowser.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/browser/SubsystemBrowser.kt
@@ -9,6 +9,8 @@
 
 package space.celestia.celestiaui.browser
 
+import space.celestia.mobilecelestia.R
+
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.togetherWith

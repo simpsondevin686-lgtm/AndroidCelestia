b541190891ecabe7bc89e28f33ed45e3d64087a0
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 16:51:24 +0300

    revert: restore CelestiaUI/src/main/java/space/celestia/celestiaui/settings/TimeSettingsScreen.kt to initial fork state

--- CelestiaUI/src/main/java/space/celestia/celestiaui/settings/TimeSettingsScreen.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/settings/TimeSettingsScreen.kt
@@ -9,8 +9,6 @@
 
 package space.celestia.celestiaui.settings
 
-import space.celestia.mobilecelestia.R
-
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues

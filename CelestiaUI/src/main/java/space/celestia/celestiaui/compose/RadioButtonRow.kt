093f03c3520ef6e6debb2ae98d2fe39dffdd24ce
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 15:29:39 +0300

    fix(ui): inject missing R import in RadioButtonRow.kt

--- CelestiaUI/src/main/java/space/celestia/celestiaui/compose/RadioButtonRow.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/compose/RadioButtonRow.kt
@@ -1,5 +1,7 @@
 package space.celestia.celestiaui.compose
 
+import space.celestia.mobilecelestia.R
+
 import android.graphics.Typeface
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row

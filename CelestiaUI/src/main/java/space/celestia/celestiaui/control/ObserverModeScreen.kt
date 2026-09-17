31ef5350551169b20b3e56e150975b40a264ad91
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 15:29:58 +0300

    fix(ui): inject missing R import in ObserverModeScreen.kt

--- CelestiaUI/src/main/java/space/celestia/celestiaui/control/ObserverModeScreen.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/control/ObserverModeScreen.kt
@@ -1,5 +1,7 @@
 package space.celestia.celestiaui.control
 
+import space.celestia.mobilecelestia.R
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer

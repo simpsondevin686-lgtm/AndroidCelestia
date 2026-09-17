8af6e646fbdb81746a2efec71f2c7bb73d5655e2
simpsondevin686-lgtm <simpsondevin686@gmail.com>
2026-09-17 16:49:43 +0300

    revert: restore CelestiaUI/src/main/java/space/celestia/celestiaui/compose/LinkPreview.kt to initial fork state

--- CelestiaUI/src/main/java/space/celestia/celestiaui/compose/LinkPreview.kt
+++ CelestiaUI/src/main/java/space/celestia/celestiaui/compose/LinkPreview.kt
@@ -1,7 +1,5 @@
 package space.celestia.celestiaui.compose
 
-import space.celestia.mobilecelestia.R
-
 import android.annotation.SuppressLint
 import android.graphics.BitmapFactory
 import androidx.compose.foundation.clickable
